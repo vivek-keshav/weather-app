1734b6423195624d813817dc816301a6b2025e36
vivek keshav <developervivek03@gmail.com>
2022-04-05 14:00:32 +0530

    forecast is updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,6 +7,11 @@ const forecast = (latitude,longitude,callback)=>{
     const url ='https://api.darksky.net/forecast/97efc916259805f85a7ad8a5d10c6561/'+latitude+','+longitude
     request({url: url  , json: true} , (error,response)=> {
 
+        const celsius= (response.body.currently.temperature -32)*5/9
+        const celsiusTemp= celsius.toFixed(2)
+        
+
+
         if(error) {
     
             callback('unable to connect weather service', undefined)
@@ -15,9 +20,9 @@ const forecast = (latitude,longitude,callback)=>{
             callback('unable to find location' , undefined)
         }else{
             // console.log(response.body.daily.data[0])
-            callback(undefined ,response.body.currently.icon+ ',' +'and  temp is ' +response.body.currently.temperature + ' F' + '  ' +  'and windSpeed is ' + response.body.currently.windSpeed + ' '  + 'km/h')
+            callback(undefined ,response.body.currently.icon+ ',' +'and  temp is ' +celsiusTemp + ' C°' + '  ' +  'and windSpeed is ' + response.body.currently.windSpeed + ' '  + 'km/h')
                 
-                console.log(response.body.currently.icon+ ',' +'and  temp is ' +response.body.currently.temperature + ' F' + '   '+ 'And windSpeed is' + response.body.currently.windSpeed )
+                console.log(response.body.currently.icon+ ',' +'and  temp is ' +celsiusTemp + '  C° ' + '   '+ 'And windSpeed is' +  response.body.currently.windSpeed )
            
             // callback(undefined ,)'
             
@@ -28,6 +33,7 @@ const forecast = (latitude,longitude,callback)=>{
 
 }
  module.exports = forecast
+
 
 
 
